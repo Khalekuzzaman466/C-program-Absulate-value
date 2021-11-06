57d7acf45ac8e986c763c7ddc4b5e9b533d4102b
কাওছার <89036436+Khalekuzzaman466@users.noreply.github.com>
2021-11-06 14:48:37 +0600

    Only c program

---
+++ Absulet value.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int number,result;
+    printf("Enter any number:");
+    scanf("%d",&number);
+    result = abs(number);
+    printf("Integer number is:%d\n",result);
+    getch();
+
+}
